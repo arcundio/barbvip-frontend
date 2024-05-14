1ac1a60f36ab831f8c835be72b80dd8d345cec4f
arcundio <santiagoarciniegasc@gmail.com>
2024-05-13 22:39:32 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './pagina/inicio/inicio.component';
+import { LoginComponent } from './pagina/login/login.component';
+import { RegistroComponent } from './pagina/registro/registro.component';
+import { VerCursosComponent } from './pagina/ver-cursos/ver-cursos.component';
+import { VerServiciosComponent } from './pagina/ver-servicios/ver-servicios.component';
+import { CitasComponent } from './pagina/citas/citas.component';
+import { InscripcionesComponent } from './pagina/inscripciones/inscripciones.component';
+import { PagoComponent } from './pagina/pago/pago.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    LoginComponent,
+    RegistroComponent,
+    VerCursosComponent,
+    VerServiciosComponent,
+    CitasComponent,
+    InscripcionesComponent,
+    PagoComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pagina/ver-servicios/ver-servicios.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ver-servicios',
+  templateUrl: './ver-servicios.component.html',
+  styleUrl: './ver-servicios.component.css'
+})
+export class VerServiciosComponent {
+
+}
