09e5ad561db499c2cc54a7e95838039b1500a71f
AlejandroZaSa <az0031456@gmail.com>
2024-05-25 11:28:12 -0500

    inicio modificado

--- src/app/pagina/inicio/inicio.component.ts
+++ src/app/pagina/inicio/inicio.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { BarberiaServicio } from '../../servicios/barberia_servicio';
 import { CursoDTO } from '../../modelo/admin/curso-dto';
 import { Alerta } from '../../modelo/otros/alerta';
+import { ServicioDTO } from '../../modelo/admin/servicio-dto';
 
 @Component({
   selector: 'app-inicio',
@@ -11,12 +12,15 @@ import { Alerta } from '../../modelo/otros/alerta';
 export class InicioComponent {
 
   cursos: CursoDTO[]
+  servicios: ServicioDTO[];
   alerta!: Alerta;
   isLoggedIn: boolean = true;
 
   constructor(private barberia: BarberiaServicio) {
     this.cursos=[]
+    this.servicios=[];
     this.cargarCursos()
+    this.cargarServicios();
   }
 
   public cargarCursos(){
@@ -24,6 +28,19 @@ export class InicioComponent {
     this.barberia.listarCursos().subscribe({
       next: data => {
         this.cursos = data.respuesta;
+  
+      },
+      error: error => {
+        this.alerta = { mensaje: error.error.respuesta, tipo: "danger" };
+      }
+    });
+  }
+
+  public cargarServicios(){
+  
+    this.barberia.listarServicios().subscribe({
+      next: data => {
+        this.servicios = data.respuesta;
   
       },
       error: error => {
